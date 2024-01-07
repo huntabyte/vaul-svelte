4fc104f430be2f36dde543c4bcf9c3a5afae2610
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2024-01-07 00:36:58 -0500

    fix: closing and focus issues (#18)

--- src/lib/internal/types.ts
+++ src/lib/internal/types.ts
@@ -1,3 +1,5 @@
+import type { Action } from 'svelte/action';
+
 export type SvelteEvent<T extends Event = Event, U extends EventTarget = EventTarget> = T & {
 	currentTarget: EventTarget & U;
 };
@@ -11,3 +13,15 @@ export type Expand<T> = T extends object
 		? { [K in keyof O]: O[K] }
 		: never
 	: T;
+
+export type Builder<
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	Element = any,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	Param = any,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	Attributes extends Record<string, any> = Record<string, any>
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+> = Record<string, any> & {
+	action: Action<Element, Param, Attributes>;
+};
